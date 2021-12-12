f434fc0e7dfb007f7048da14d6705be40f1ed5f6
JorG <>
2021-12-11 18:30:58 -0600

    API endpoints for products, reviews, and creating a review

--- backend/index.js
+++ backend/index.js
@@ -4,18 +4,83 @@ const express = require('express')
 const app = express()
 const knexFile = require('./knexfile')
 
+// Parse JSON
+app.use(express.json())
+
+
 // Add database to request's context
 app.use(function(req, res, next) {
   req.database = require('knex')(knexFile)
   next()
 })
 
+
+// API endpoints
 app.get('/products', async (req, res) => {
-  res.send('Hello World!')
+  try {
+    const rows = await req.database('products').select('*')
+    return res.send(rows)
+  } catch (err) {
+    return res.status(500).send({
+      message: 'Failed to get products',
+    })
+  }
 })
 
-app.get('/reviews', (req, res) => {
-  res.send('Hello World!')
+app.get('/reviews', async (req, res) => {
+  const { productId } = req.params
+  if (!productId) {
+    return res.status(400).send({
+      message: 'Invalid product ID',
+    })
+  }
+
+  try {
+    const results = await req.database('reviews').where({ productId })
+    return res.send(results)
+  } catch (err) {
+    return res.status(500).send({
+      message: 'Failed to get reviews',
+    })
+  }
+})
+
+app.post('/reviews', async function(req, res) {
+  const { body } = req
+
+  // Validate input
+  if (!body) {
+    return res.status(400).send({
+      message: 'Invalid input',
+    })
+  }
+
+  if (typeof body.productId !== 'number') {
+    return res.status(400).send({
+      message: 'Invalid product ID',
+    })
+  }
+
+  if (typeof body.rating !== 'number' || body.rating < 1 || body.rating > 5) {
+    return res.status(400).send({
+      message: 'Invalid rating',
+    })
+  }
+
+  try {
+    const result = await req.database('reviews').insert({
+      product_id: body.productId,
+      rating: body.rating,
+      comment: body.comment,
+    })
+    return res.status(201).send({
+      inserted: result.rowCount,
+    })
+  } catch (err) {
+    return res.status(500).send({
+      message: 'Failed to create review',
+    })
+  }
 })
 
 
