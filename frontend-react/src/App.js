81226f4452047d3744b6ec8870f538695757bb22
Stoic <1910135+stoicskyline@users.noreply.github.com>
2021-12-12 19:52:08 -0600

    add Product, ExistingReview components and hooks

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -40,7 +40,7 @@ const ReviewModal = () => {
   )
 }
 
-const useProducts = (videogameSlug) => {
+const useProducts = () => {
   const [products, setProducts] = React.useState([])
   const [error, setError] = React.useState()
   const [loading, setLoading] = React.useState(true)
@@ -75,6 +75,107 @@ const useProducts = (videogameSlug) => {
   }
 }
 
+const useReviews = (productId) => {
+  const [reviews, setReviews] = React.useState([])
+  const [error, setError] = React.useState()
+  const [loading, setLoading] = React.useState(true)
+
+  React.useEffect(() => {
+    if (productId) {
+      setLoading(true)
+      fetch(`${API_URL}/reviews?productId=${productId}`)
+        .then(response => response.json())
+        .then((data) => {
+          setReviews(data)
+        })
+        .catch(err => {
+          setError(err)
+        })
+        .then(() => {
+          setLoading(false)
+        })
+    } else {
+      setReviews([])
+    }
+
+    return () => {
+      setReviews([])
+      setError(undefined)
+      setLoading(false)
+    }
+  }, [productId])
+
+  return {
+    error,
+    loading,
+    reviews,
+  }
+}
+
+const ExistingReview = ({ review }) => {
+  // TODO
+
+  return (
+    <div className="d-flex justify-content-start align-content-center">
+
+    </div>
+  )
+}
+
+const Product = ({ product }) => {
+  const {
+    error,
+    loading,
+    reviews,
+  } = useReviews(product.id)
+
+  if (!product || !product.id) {
+    return (
+      <h4 className="error-message">
+        No product found
+      </h4>
+    )
+  }
+
+  return (
+    <>
+      <h1 id="product-name">{product.name || '???'}</h1>
+      <div className="d-flex justify-content-between align-content-center rating-box">
+        <div id="average-rating" className="d-flex align-content-start">
+          <div className="rating-number">
+            1.0
+          </div>
+          <div id="average-stars" className="stars d-flex justify-content-start align-content-center">
+            <div className="star"></div>
+            <div className="star"></div>
+            <div className="star"></div>
+            <div className="star"></div>
+            <div className="star"></div>
+          </div>
+        </div>
+        <div className="d-flex align-content-center">
+          <button
+            id="add-review"
+            type="button"
+            className="btn btn-outline-secondary review-button"
+            data-bs-toggle="modal"
+            data-bs-target="#review-modal"
+          >
+            Add review
+          </button>
+        </div>
+      </div>
+
+      <h4 className="existing-reviews__header">Reviews</h4>
+      <div id="existing-reviews">
+        {reviews.map(review => (
+          <ExistingReview review={review} />
+        ))}
+      </div>
+    </>
+  )
+}
+
 const App = () => {
   const {
     error,
@@ -82,44 +183,18 @@ const App = () => {
     products,
   } = useProducts()
 
-  console.log(error)
-  console.log(products)
+  const product = products && products[0]
 
   return (
     <div className="d-flex justify-content-center flex-wrap main-box">
       <div id="current-product" className="card main-card">
         <div className="card-body">
           {error && <h4 className="error-message">{`${error}`}</h4>}
-
-          <h1 id="product-name">Loading ...</h1>
-          <div className="d-flex justify-content-between align-content-center rating-box">
-            <div id="average-rating" className="d-flex align-content-start">
-              <div className="rating-number">
-                1.0
-              </div>
-              <div id="average-stars" className="stars d-flex justify-content-start align-content-center">
-                <div className="star"></div>
-                <div className="star"></div>
-                <div className="star"></div>
-                <div className="star"></div>
-                <div className="star"></div>
-              </div>
-            </div>
-            <div className="d-flex align-content-center">
-              <button
-                id="add-review"
-                type="button"
-                className="btn btn-outline-secondary review-button"
-                data-bs-toggle="modal"
-                data-bs-target="#review-modal"
-              >
-                Add review
-              </button>
-            </div>
-          </div>
-
-          <h4 className="existing-reviews__header">Reviews</h4>
-          <div id="existing-reviews"></div>
+          {loading ? (
+            <h1 id="product-name">Loading ...</h1>
+          ) : (
+            <Product product={product} />
+          )}
         </div>
       </div>
     </div>
